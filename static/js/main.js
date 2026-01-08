6033d01b41e744af0ec13766026796147b2a894e
ltam0127 <ltam0127@uni.sydney.edu.au>
2026-01-08 17:25:44 +0800

    Modify flash error display and auto disappear timing

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,13 @@
-// Custom JS will go here later
+// Custom JS for the AGM Voting Platform
 console.log("AGM Voting Platform loaded");
+
+// Auto-dismiss Bootstrap alerts after 3 seconds
+document.addEventListener('DOMContentLoaded', function () {
+	var alerts = document.querySelectorAll('.alert.alert-dismissible.show');
+	alerts.forEach(function (alert) {
+		setTimeout(function () {
+			var bsAlert = bootstrap.Alert.getOrCreateInstance(alert);
+			bsAlert.close();
+		}, 3000);
+	});
+});
